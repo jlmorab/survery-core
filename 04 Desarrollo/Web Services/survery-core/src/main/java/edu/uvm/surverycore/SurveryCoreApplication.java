86bacdd50f50ee284bd8438b371a3d824bc9d42f
José Luis Mora <jlmorab@hotmail.com>
2017-06-08 23:58:19 -0500

    Creación inicial de proyecto

---
+++ 04 Desarrollo/Web Services/survery-core/src/main/java/edu/uvm/surverycore/SurveryCoreApplication.java
@@ -0,0 +1,12 @@
+package edu.uvm.surverycore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveryCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveryCoreApplication.class, args);
+	}
+}
